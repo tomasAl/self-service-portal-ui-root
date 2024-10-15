5f295f856e2eed494e2e5ee713ff2ed029bf09df
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-10-15 09:30:23 +0300

    chore: utility functions documentation

--- src/utils/routing.ts
+++ src/utils/routing.ts
@@ -1,21 +1,32 @@
 import { navigateToUrl } from "single-spa";
 import env from "../env";
 
+/*
+ * Funkcija, skirta nukreipti naudotoją į savitarnos krepšelio puslapį.
+ */
 export const redirectToCart = (): void =>
   navigateToUrl(`${env.ENV_PUBLIC_PORTAL_URL}/krepselis`);
 
+/*
+ * Funkcija, skirta nukreipti naudotoją į viešojo portalo titulinį puslapį.
+ */
 export const redirectToLanding = (): void =>
   navigateToUrl(env.ENV_PUBLIC_PORTAL_URL);
 
+/*
+ * Funkcija, skirta nukreipti naudotoją į savitarnos titulinį puslapį.
+ */
 export const redirectToSelfServiceDashboard = (): void =>
   navigateToUrl(`${env.ENV_PUBLIC_PORTAL_URL}/savitarna`);
 
+/*
+ * Funkcija, skirta nukreipti naudotoją į paslaugos aprašymo puslapį.
+ */
 export const redirectToServiceDescriptionPage = (id: string): void =>
   navigateToUrl(`${env.ENV_PUBLIC_PORTAL_URL}/paslauga/${id}`);
 
 /*
- * Funkcija skirta nukreipti naudotoją į paslaugos aprašymo puslapį su klaidos pranešimu,
- * informuojančiu apie tai, kad paslauga negali būti teikiama klientui, prisijungus tam tikru vaidmeniu.
+ * Funkcija, skirta nukreipti naudotoją į paslaugos aprašymo puslapį su klaidos pranešimu.
  */
 export const unsupportedServiceRedirect = (id: string, message: string): void => {
   const urlParams = new URLSearchParams();
